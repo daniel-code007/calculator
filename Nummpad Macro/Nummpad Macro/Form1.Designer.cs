a2ea07b6e56fbdefbc40c234d21b5a18cc7a8310
daniel-code007 <69769229+daniel-code007@users.noreply.github.com>
2020-09-04 22:48:18 +0200

    Add files via upload
    the nummpad macro design

---
+++ Nummpad Macro/Nummpad Macro/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Nummpad_Macro
+{
+    partial class NumMacPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Macro1 = new System.Windows.Forms.Button();
+            this.Macro2 = new System.Windows.Forms.Button();
+            this.Macro3 = new System.Windows.Forms.Button();
+            this.Macro4 = new System.Windows.Forms.Button();
+            this.Macro5 = new System.Windows.Forms.Button();
+            this.Macro6 = new System.Windows.Forms.Button();
+            this.Macro7 = new System.Windows.Forms.Button();
+            this.Macro8 = new System.Windows.Forms.Button();
+            this.Macro9 = new System.Windows.Forms.Button();
+            this.MacroSwitch = new System.Windows.Forms.CheckBox();
+            this.Recorder = new System.Windows.Forms.ListBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.StartRec = new System.Windows.Forms.Button();
+            this.StopRec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Macro1
+            // 
+            this.Macro1.Location = new System.Drawing.Point(12, 48);
+            this.Macro1.Name = "Macro1";
+            this.Macro1.Size = new System.Drawing.Size(75, 23);
+            this.Macro1.TabIndex = 0;
+            this.Macro1.Text = "Macro1";
+            this.Macro1.UseVisualStyleBackColor = true;
+            this.Macro1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Macro2
+            // 
+            this.Macro2.Location = new System.Drawing.Point(12, 78);
+            this.Macro2.Name = "Macro2";
+            this.Macro2.Size = new System.Drawing.Size(75, 23);
+            this.Macro2.TabIndex = 1;
+            this.Macro2.Text = "Macro2";
+            this.Macro2.UseVisualStyleBackColor = true;
+            // 
+            // Macro3
+            // 
+            this.Macro3.Location = new System.Drawing.Point(12, 108);
+            this.Macro3.Name = "Macro3";
+            this.Macro3.Size = new System.Drawing.Size(75, 23);
+            this.Macro3.TabIndex = 2;
+            this.Macro3.Text = "Macro3";
+            this.Macro3.UseVisualStyleBackColor = true;
+            // 
+            // Macro4
+            // 
+            this.Macro4.Location = new System.Drawing.Point(12, 138);
+            this.Macro4.Name = "Macro4";
+            this.Macro4.Size = new System.Drawing.Size(75, 23);
+            this.Macro4.TabIndex = 3;
+            this.Macro4.Text = "Macro4";
+            this.Macro4.UseVisualStyleBackColor = true;
+            // 
+            // Macro5
+            // 
+            this.Macro5.Location = new System.Drawing.Point(12, 168);
+            this.Macro5.Name = "Macro5";
+            this.Macro5.Size = new System.Drawing.Size(75, 23);
+            this.Macro5.TabIndex = 4;
+            this.Macro5.Text = "Macro5";
+            this.Macro5.UseVisualStyleBackColor = true;
+            // 
+            // Macro6
+            // 
+            this.Macro6.Location = new System.Drawing.Point(12, 198);
+            this.Macro6.Name = "Macro6";
+            this.Macro6.Size = new System.Drawing.Size(75, 23);
+            this.Macro6.TabIndex = 5;
+            this.Macro6.Text = "Macro6";
+            this.Macro6.UseVisualStyleBackColor = true;
+            // 
+            // Macro7
+            // 
+            this.Macro7.Location = new System.Drawing.Point(12, 228);
+            this.Macro7.Name = "Macro7";
+            this.Macro7.Size = new System.Drawing.Size(75, 23);
+            this.Macro7.TabIndex = 6;
+            this.Macro7.Text = "Macro7";
+            this.Macro7.UseVisualStyleBackColor = true;
+            // 
+            // Macro8
+            // 
+            this.Macro8.Location = new System.Drawing.Point(12, 258);
+            this.Macro8.Name = "Macro8";
+            this.Macro8.Size = new System.Drawing.Size(75, 23);
+            this.Macro8.TabIndex = 7;
+            this.Macro8.Text = "Macro8";
+            this.Macro8.UseVisualStyleBackColor = true;
+            // 
+            // Macro9
+            // 
+            this.Macro9.Location = new System.Drawing.Point(12, 288);
+            this.Macro9.Name = "Macro9";
+            this.Macro9.Size = new System.Drawing.Size(75, 23);
+            this.Macro9.TabIndex = 8;
+            this.Macro9.Text = "Macro9";
+            this.Macro9.UseVisualStyleBackColor = true;
+            // 
+            // MacroSwitch
+            // 
+            this.MacroSwitch.AutoSize = true;
+            this.MacroSwitch.Location = new System.Drawing.Point(13, 13);
+            this.MacroSwitch.Name = "MacroSwitch";
+            this.MacroSwitch.Size = new System.Drawing.Size(122, 17);
+            this.MacroSwitch.TabIndex = 9;
+            this.MacroSwitch.Text = "De/activate macro\'s";
+            this.MacroSwitch.UseVisualStyleBackColor = true;
+            // 
+            // Recorder
+            // 
+            this.Recorder.FormattingEnabled = true;
+            this.Recorder.Location = new System.Drawing.Point(93, 47);
+            this.Recorder.Name = "Recorder";
+            this.Recorder.Size = new System.Drawing.Size(157, 264);
+            this.Recorder.TabIndex = 10;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(173, 13);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 11;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // StartRec
+            // 
+            this.StartRec.Location = new System.Drawing.Point(93, 318);
+            this.StartRec.Name = "StartRec";
+            this.StartRec.Size = new System.Drawing.Size(75, 23);
+            this.StartRec.TabIndex = 12;
+            this.StartRec.Text = "Start";
+            this.StartRec.UseVisualStyleBackColor = true;
+            // 
+            // StopRec
+            // 
+            this.StopRec.Location = new System.Drawing.Point(175, 317);
+            this.StopRec.Name = "StopRec";
+            this.StopRec.Size = new System.Drawing.Size(75, 23);
+            this.StopRec.TabIndex = 13;
+            this.StopRec.Text = "Stop";
+            this.StopRec.UseVisualStyleBackColor = true;
+            // 
+            // NumMacPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 343);
+            this.Controls.Add(this.StopRec);
+            this.Controls.Add(this.StartRec);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.Recorder);
+            this.Controls.Add(this.MacroSwitch);
+            this.Controls.Add(this.Macro9);
+            this.Controls.Add(this.Macro8);
+            this.Controls.Add(this.Macro7);
+            this.Controls.Add(this.Macro6);
+            this.Controls.Add(this.Macro5);
+            this.Controls.Add(this.Macro4);
+            this.Controls.Add(this.Macro3);
+            this.Controls.Add(this.Macro2);
+            this.Controls.Add(this.Macro1);
+            this.Name = "NumMacPad";
+            this.Text = "Nummpad macro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Macro1;
+        private System.Windows.Forms.Button Macro2;
+        private System.Windows.Forms.Button Macro3;
+        private System.Windows.Forms.Button Macro4;
+        private System.Windows.Forms.Button Macro5;
+        private System.Windows.Forms.Button Macro6;
+        private System.Windows.Forms.Button Macro7;
+        private System.Windows.Forms.Button Macro8;
+        private System.Windows.Forms.Button Macro9;
+        private System.Windows.Forms.CheckBox MacroSwitch;
+        private System.Windows.Forms.ListBox Recorder;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button StartRec;
+        private System.Windows.Forms.Button StopRec;
+    }
+}
+
